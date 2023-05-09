30792318d7fdc20ac8a9d4549058031a64b60473
mmolina727 <matiasgmolina.unicen@gmail.com>
2023-05-09 03:35:36 -0300

    Se implementa el metodo BFS

--- ServicioBFS.java
+++ ServicioBFS.java
@@ -3,21 +3,58 @@ package ej1;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class ServicioBFS {
 
 	private Grafo<?> grafo;
-	private ArrayList<Integer>marcado;
+	private HashMap<Integer, String> vertice; 
 	
 	public ServicioBFS(Grafo<?> grafo) {
 		this.grafo = grafo;
-		marcado= new ArrayList<>();
+		vertice= new HashMap<>();
 	}
 	
 	public List<Integer> bfsForest() {
+		ArrayList<Integer>marcado= new ArrayList<>();
 		
+		Iterator<Integer>vert= grafo.obtenerVertices();
 		
-		return new ArrayList<>();
+		while(vert.hasNext()) {
+			int v= vert.next();
+			vertice.put(v, "NO VISITADO");				
+		}
+		for (Integer v : vertice.keySet()) {
+			if(vertice.get(v).equals("NO VISITADO")) {
+				marcado.addAll(bfsVisit(v));
+			}
+		}		
+		
+		return marcado;
 	}
 	
+	private ArrayList<Integer>bfsVisit(int v){
+		ArrayList<Integer>recorrido= new ArrayList<>();
+		Queue<Integer>vertLevel= new LinkedList<>();
+		recorrido.add(v);
+		vertice.put(v, "VISITADO");
+		vertLevel.add(v);
+		while(!vertLevel.isEmpty()) {
+			int elemento=vertLevel.peek();
+			vertLevel.poll();
+			Iterator<Integer>vertAdya= grafo.obtenerAdyacentes(elemento);
+			
+			while(vertAdya.hasNext()) {
+				int adyacente= vertAdya.next();	
+				if(vertice.get(adyacente).equals("NO VISITADO")) {
+					vertice.put(adyacente, "VISITADO");
+					vertLevel.add(adyacente);
+					recorrido.add(adyacente);
+				}
+			}
+		}
+		return recorrido;
+	}
 }
